da0fec34d179b3ba6ee1fdf0e7f823bf3129c4f1
MasatoIso <04masato05@gmail.com>
2021-09-23 20:44:38 +0900

    fetch cat image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [catImageUrl, setCatImageUrl] = useState("");
+
+  const fetchCatInfo = async () => {
+    await fetch('https://api.thecatapi.com/v1/images/search')
+      .then(response => response.json())
+      .then(data => setCatImageUrl(data[0].url))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button type="button" onClick={fetchCatInfo}>Click!</button>
+      <img src={catImageUrl ?? ""} />
     </div>
   );
 }
